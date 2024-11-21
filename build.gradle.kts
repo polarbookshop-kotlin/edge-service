ae559bf27a926315a51324cf0087a30fccd62f03
Ruibin Zhang <ruibin.zhang021@icloud.com>
2024-11-21 15:23:46 +0800

    initialzie the project

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.polarbookshop"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+extra["testcontainersVersion"] = "1.17.3"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	implementation("org.springframework.session:spring-session-data-redis")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+		mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
+	}
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.named<BootBuildImage>("bootBuildImage"){
+	builder = "docker.io/paketobuildpacks/builder-jammy-base"
+	imageName = project.name
+	environment = mapOf("BP_JVM_VERSION" to "17")
+	docker {
+		publishRegistry {
+			username = project.findProperty("registryUsername") as String?
+			password= project.findProperty("registryToken") as String?
+			url=project.findProperty("registryUrl") as String?
+		}
+	}
+}
