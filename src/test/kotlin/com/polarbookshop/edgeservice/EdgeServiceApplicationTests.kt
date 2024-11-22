30c355d49c70c84e9c734d2a5fcf475bc83b8ff2
Ruibin Zhang <ruibin.zhang021@icloud.com>
2024-11-22 22:10:31 +0800

    add mock bean for test contest load

--- src/test/kotlin/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.kt
+++ src/test/kotlin/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.kt
@@ -2,12 +2,16 @@ package com.polarbookshop.edgeservice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -16,8 +20,12 @@ class EdgeServiceApplicationTests {
 		const val REDIS_PORT = 6379
 
 		@Container
+		@ServiceConnection
 		val redis: GenericContainer<*> = GenericContainer(DockerImageName.parse("redis:7.0"))
 			.withExposedPorts(REDIS_PORT)
+
+		@MockBean
+		var clientRegistrationRepository: ReactiveClientRegistrationRepository? = null
 
 		@DynamicPropertySource
 		fun redisProperties(registry: DynamicPropertyRegistry){
